2cb75a2d1d971b9a57685323fbd1e96cb050f01b
Anna-Voronina <anna.vs2512@gmail.com>
2023-09-21 11:27:34 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import SharedLayout from "./shared/components/SharedLayout/SharedLayout";
+import MainPage from "./pages/MainPage/MainPage";
+
+const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
+const NoticesPage = lazy(() => import("./pages/NoticesPage/NoticesPage"));
+const UserPage = lazy(() => import("./pages/UserPage/UserPage"));
+const AddPetPage = lazy(() => import("./pages/AddPetPage/AddPetPage"));
+const OurFriendsPage = lazy(() =>
+  import("./pages/OurFriendsPage/OurFriendsPage")
+);
+const NewsPage = lazy(() => import("./pages/NewsPage/NewsPage"));
+
 function App() {
-  return <div>Testifgf</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<MainPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="notices/:categoryName" element={<NoticesPage />} />
+        <Route path="user" element={<UserPage />} />
+        <Route path="add-pet" element={<AddPetPage />} />
+        <Route path="friends" element={<OurFriendsPage />} />
+        <Route path="news" element={<NewsPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;

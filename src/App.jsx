ffa6a5bbc5bef88d2b255b1fd3e70cecab952eaf
Anna-Voronina <anna.vs2512@gmail.com>
2023-09-26 23:09:59 +0300

    add logout and fetching current user

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import { lazy } from "react";
+import { lazy, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import SharedLayout from "./shared/components/SharedLayout/SharedLayout";
 import MainPage from "./pages/MainPage/MainPage";
+import { useDispatch, useSelector } from "react-redux";
+import { selectRefresh } from "./redux/auth/authSelectors";
+import { refreshThunk } from "./redux/auth/authOperations";
+import LoadingPage from "./pages/LoadingPage/LoadingPage";
 
 const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
 const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
@@ -15,20 +19,33 @@ const NewsPage = lazy(() => import("./pages/NewsPage/NewsPage"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 
 function App() {
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectRefresh);
+
+  useEffect(() => {
+    dispatch(refreshThunk());
+  }, [dispatch]);
+
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<MainPage />} />
-        <Route path="register" element={<RegisterPage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="notices" element={<NoticesPage />} />
-        <Route path="user" element={<UserPage />} />
-        <Route path="add-pet" element={<AddPetPage />} />
-        <Route path="friends" element={<OurFriendsPage />} />
-        <Route path="news" element={<NewsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+    <>
+      {isRefreshing ? (
+        <LoadingPage />
+      ) : (
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<MainPage />} />
+            <Route path="register" element={<RegisterPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="notices" element={<NoticesPage />} />
+            <Route path="user" element={<UserPage />} />
+            <Route path="add-pet" element={<AddPetPage />} />
+            <Route path="friends" element={<OurFriendsPage />} />
+            <Route path="news" element={<NewsPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      )}
+    </>
   );
 }
 

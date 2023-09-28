49746f678136bf6b8b5a2cc4440382cb37d04c8b
Anna-Voronina <anna.vs2512@gmail.com>
2023-09-28 14:17:14 +0300

    add notices requests

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -79,6 +79,11 @@ export const fetchNews = ({ page = 1, limit = 6 }) => {
   return data;
 };
 
+export const fetchNewsBySearch = async (searchQuery) => {
+  const data = connectionsApi.get(`/news/${searchQuery}`);
+  return data;
+};
+
 export const fetchFriends = ({ page = 1, limit = 6 }) => {
   const data = connectionsApi.get("/friends", {
     params: {
@@ -102,5 +107,96 @@ export const addNewPet = (pet) => {
 
 export const deletePet = (petId) => {
   const data = connectionsApi.delete(`/pets/${petId}`);
+  return data;
+};
+
+// ------- PUBLIC ------- //
+
+export const fetchNoticesBySearch = async ({
+  page = 1,
+  limit = 12,
+  searchQuery,
+}) => {
+  const data = connectionsApi.get(`/notices/query/${searchQuery}`, {
+    params: {
+      page,
+      limit,
+    },
+  });
+  return data;
+};
+
+export const fetchNoticesByCategory = async (category) => {
+  const data = connectionsApi.get(`/notices/search/${category}`);
+  return data;
+};
+
+export const fetchNoticesByCategoryAndSearch = async ({
+  category,
+  searchQuery,
+}) => {
+  const data = connectionsApi.get(`/notices/search/${category}/${searchQuery}`);
+  return data;
+};
+
+export const fetchNoticeById = async (noticeId) => {
+  const data = connectionsApi.get(`/notices/search/${noticeId}`);
+  return data;
+};
+
+export const fetchFilteredNotices = async ({
+  age = "1",
+  sex,
+  category = "sell",
+}) => {
+  const data = connectionsApi.get("/filter/by", {
+    params: {
+      age,
+      sex,
+      category,
+    },
+  });
+  return data;
+};
+
+// ------- PRIVATE ------- //
+
+export const fetchAllNotices = async ({ page = 1, limit = 12 }) => {
+  const data = connectionsApi.get("/notices/get/all", {
+    params: {
+      page,
+      limit,
+    },
+  });
+  return data;
+};
+
+export const fetchOwnNotices = async ({ page = 1, limit = 12 }) => {
+  const data = connectionsApi.get("/notices", {
+    params: {
+      page,
+      limit,
+    },
+  });
+  return data;
+};
+
+export const fetchFavoriteNotices = async () => {
+  const data = connectionsApi.get("/notices/get/favorites");
+  return data;
+};
+
+export const addNewNotice = (notice) => {
+  const data = connectionsApi.post("/notices", notice);
+  return data;
+};
+
+export const addOrDeleteFavoriteNotice = (noticeId) => {
+  const data = connectionsApi.patch(`/notices/${noticeId}/favorite`);
+  return data;
+};
+
+export const deleteOwnNotice = (noticeId) => {
+  const data = connectionsApi.delete(`/notices/${noticeId}`);
   return data;
 };

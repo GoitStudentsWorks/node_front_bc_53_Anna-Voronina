b85f245d1d2792d8f96db0de8aa3cf1e65395a5e
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-09-29 10:11:27 +0300

    Update Pagination.jsx

--- src/shared/components/Pagination/Pagination.jsx
+++ src/shared/components/Pagination/Pagination.jsx
@@ -3,15 +3,15 @@ import ReactPaginate from "react-paginate";
 import { useMediaQuery } from "react-responsive";
 import { PaginationContainer } from "./Pagination.styled";
 
-export const Pagination = ({ onPageChange, totalPets, currentPage }) => {
+export const Pagination = ({
+  onPageChange,
+  totalPets,
+  currentPage,
+  perPage,
+}) => {
   const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
-  const isTablet = useMediaQuery({
-    query: "(min-width: 768px, max-width: 1279px)",
-  });
-  const isDesktop = useMediaQuery({ query: "(min-width: 1279px)" });
 
   const marginPages = isMobile ? 1 : 2;
-  const perPage = (isMobile && 11) || (isTablet && 10) || (isDesktop && 12);
 
   const pageCount = Math.ceil(totalPets / perPage);
 
@@ -45,7 +45,8 @@ export const Pagination = ({ onPageChange, totalPets, currentPage }) => {
 };
 
 Pagination.propTypes = {
+  perPage: PropTypes.number,
   currentPage: PropTypes.number,
-  onPageChange: PropTypes.func,
   totalPets: PropTypes.number,
+  onPageChange: PropTypes.func,
 };

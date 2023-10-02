b4f7bc0b4297d4ffc920d413bd8d3681d6ddaaea
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-10-02 11:21:34 +0300

    UPDATED Form styles

--- src/shared/components/Logout/Logout.styled.js
+++ src/shared/components/Logout/Logout.styled.js
@@ -27,7 +27,7 @@ export const LogoutBtn = styled(ButtonStyled)`
   ${({ $variant }) =>
     $variant === "profile" &&
     css`
-      padding: 8px 0;
+      padding: 0;
       margin-top: 16px;
       flex-direction: row-reverse;
       gap: 12px;
@@ -38,15 +38,12 @@ export const LogoutBtn = styled(ButtonStyled)`
       color: ${({ theme }) => theme.colors.grey};
 
       @media only screen and (min-width: 768px) {
-        margin-top: 6px;
-        position: absolute;
-        bottom: 10px;
-        left: 23px;
+        margin: 14px 0 4px;
       }
 
       @media only screen and (min-width: 1280px) {
         position: static;
-        margin-top: 14px;
+        margin: 6px 0 0 0;
       }
     `}
 

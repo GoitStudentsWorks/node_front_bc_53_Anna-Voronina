6929f7f6e4536ab550d2d0bc7fcc6bb8cc4ba09e
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-10-03 14:19:22 +0300

    Updated: Pets list img

--- src/modules/user/components/EmptyPetsList/EmptyPetsList.jsx
+++ src/modules/user/components/EmptyPetsList/EmptyPetsList.jsx
@@ -1,10 +1,16 @@
 import pets from "../../images/pets.png";
+import pets1x from "../../images/pets@1x.webp";
+import pets2x from "../../images/pets@2x.webp";
 import { EmptyPetsImg, EmptyPetsText } from "./EmptyPetsList.styled";
 
 export const EmptyPetsList = () => {
   return (
     <>
-      <EmptyPetsImg src={pets} alt="Empty pets list" />
+      <EmptyPetsImg
+        srcSet={`${pets1x} 1x, ${pets2x} 2x`}
+        src={pets}
+        alt="Empty pets list"
+      />
       <EmptyPetsText>
         You have not added your pets, if you want to add them, click on the
         button &#34;Add pet&#34;

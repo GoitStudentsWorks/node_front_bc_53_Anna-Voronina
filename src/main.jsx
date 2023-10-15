a5df82272bbb3b12d2f1d9cace1a0dcee988c49a
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-15 15:21:17 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -17,8 +17,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <BrowserRouter 
-{/*             basename="/node_front_bc_53_Anna-Voronina/" */}
-          basename={import.meta.env.DEV ? '/' : '/node_front_bc_53_Anna-Voronina/'}
+            basename="/node_front_bc_54_Anna-Voronina/"
+{/*           basename={import.meta.env.DEV ? '/' : '/node_front_bc_54_Anna-Voronina/'} */}
             >
             <App />
             <ToastContainer autoClose={2000} />

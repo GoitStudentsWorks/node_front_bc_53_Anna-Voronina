25070124fa1cfcce554d10fa8a195505fd1dcf32
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-15 13:45:55 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="/node_front_bc_53_Anna-Voronina">
+          <BrowserRouter basename="/node_front_bc_53_Anna-Voronina/">
             <App />
             <ToastContainer autoClose={2000} />
             <GlobalStyles />

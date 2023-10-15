8f15538a283bc2579c82d601489ebaa5564e8484
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-15 13:37:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-  base: /node_front_bc_53_Anna-Voronina/
-});
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/node_front_bc_53_Anna-Voronina/'
+  }
+
+  return config
+})
